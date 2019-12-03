525ef7d9ef689710eceb05a562647e28610a596b
MingLiu <mingl19@uci.edu>
2019-12-02 18:28:36 -0800

    bug fix

--- savary_service.py
+++ savary_service.py
@@ -18,10 +18,6 @@ def abort_if_shoe_id_doesnt_exist(shoe_id):
     if shoe_id not in SHOE_IDS:
         abort(404, message="Shoe {} doesn't exist".format(shoe_id))
 
-def abort_if_shoe_title_doesnt_exist(shoe_title):
-    if shoe_title not in SHOE_TITLES:
-        abort(404, message="Shoe {} doesn't exist".format(shoe_title))
-
 class ShoeByID(Resource):
     def get(self, shoe_id):
         abort_if_shoe_id_doesnt_exist(shoe_id)
@@ -32,7 +28,6 @@ class ShoeByID(Resource):
 class ShoeByTitle(Resource):
     def get(self, shoe_title):
         res = []
-        abort_if_shoe_title_doesnt_exist(shoe_title)
         for d in SHOE_DATA:
             if (shoe_title in d['title']):
                 res.append(d)
@@ -44,7 +39,7 @@ class ShoeList(Resource):
 
 api.add_resource(ShoeList, '/all')
 api.add_resource(ShoeByID, '/id/<shoe_id>')
-api.add_resource(ShoeByTitle, '/title/<shoe_title>')
+api.add_resource(ShoeByTitle, '/title_like/<shoe_title>')
 
 if __name__ == '__main__':
     app.run(debug=True)
